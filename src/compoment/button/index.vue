9a0726ba952f45cf808e07e5c4c5588eedf57ca1
Ivan <1095036273@qq.com>
2021-07-31 22:46:19 +0800

    feat: demo

---
+++ src/compoment/button/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="index" @tap="abc">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps } from "vue";
+const msg = ref("Hello world");
+const abc = () => {
+  console.log(1111111);
+};
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="index" @tap="abc" v-if="msg">
+    <text>{{ msg }}</text>
+  </view>
+  <button type="warn" :disabled="true">1212</button>
+  <checkbox-group @change="abc">
+    <checkbox></checkbox>
+  </checkbox-group>
+  <hellow />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import hellow from "../../compoment/button/index.vue";
+
+import "./index.scss";
+
+const msg = ref("Hello world");
+const abc = () => {
+  console.log(1231);
+};
+</script>
