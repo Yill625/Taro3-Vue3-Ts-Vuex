349bd70c0e1bb63e3ad7633593158edada4aa2fe
Ivan <1095036273@qq.com>
2021-07-31 23:09:11 +0800

    feat: 点击事件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="index" @tap="abc" v-if="msg">
     <text>{{ msg }}</text>
   </view>
-  <button type="warn" :disabled="true">1212</button>
+  <button type="warn" :disabled="false" @tap="show">1212</button>
   <checkbox-group @change="abc">
     <checkbox> </checkbox>
   </checkbox-group>
@@ -15,8 +15,11 @@ import hellow from '../../compoment/button/index.vue'
 
 import './index.scss'
 
-const msg = ref('Hello world')
+const msg = ref(true)
 const abc = () => {
   console.log(1231)
+}
+const show = () => {
+  msg.value = !msg.value
 }
 </script>
